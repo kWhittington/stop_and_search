2c5413f340d196e1278e9373cc85d46ca4b53b59
Kyle Whittington <kyle.thomas.whittington@gmail.com>
2016-09-26 16:50:40 -0500

    Add circle.yml
    Custom `./bin/start`, `./bin/stop`, and `./bin/circle_c_i/test` scripts
    were added to try to get the Cucumber tests running on CircleCI.

--- server.js
+++ server.js
@@ -1,10 +1,2 @@
-const express = require('express');
-
-const dir = process.argv[2];
-const port = 8080;
-const app = express();
-
-app.use(express.static(dir));
-app.listen(port);
-
-console.log('\n Serving directory /' + dir + ' on port ' + port);
+const StopAndSearch = require('./lib/stop_and_search')
+StopAndSearch.start()
