97233787fbed8f60a7ca15d47cfb5841df8f7156
Kyle Whittington <kyle.thomas.whittington@gmail.com>
2016-11-05 14:50:44 -0500

    Place <info> content into a container
    Putting the content into a container class element will better pad it
    in-line with the widths being used elsewhere in the page.

--- app/info/component.js
+++ app/info/component.js
@@ -7,30 +7,34 @@ import ViewModel from './view_model.js'
   Knockout.components.register('info', {
     viewModel: ViewModel,
     template: `
-      <div id="about" class="ui container grid">
-        <div class="title row">
-          <h2 class="ui header">
-            <i class="info icon"></i>
-            <div class="content" data-bind="text: title()"></div>
-          </h2>
-        </div>
-        <div class="content row">
-          <div class="container">
-            <h3 class="ui header">
-              Data Source
-            </h3>
-            <p>
-              This page is backed by <a href="https://data.nola.gov">
-              Data.NOLA.gov</a>'s <a href="http://tinyurl.com/h2sq7du">
-              Stop and Search (Field Interviews)</a>. Thank you, NOPD, for
-              making this information public.
-            </p>
+      <div id="about" class="ui">
+        <h2 class="ui header">
+          <i class="info icon"></i>
+          <div class="content" data-bind="text: title()"></div>
+        </h2>
+        <div class="ui grid container">
+          <div class="one column row">
+            <div class="column">
+              <h3 class="ui header">
+                Data Source
+              </h3>
+            </div>
+          </div>
+          <div class="one column row">
+            <div class="column"
+              <p>
+                This page is backed by <a href="https://data.nola.gov">
+                Data.NOLA.gov</a>'s <a href="http://tinyurl.com/h2sq7du">
+                Stop and Search (Field Interviews)</a>. Thank you, NOPD, for
+                making this information public.
+              </p>
 
-            <p>
-              If you want to learn how to query the database yourself, check
-              out <a href="http://tinyurl.com/z9jjsjb">
-              their very helpful Socrata powered API docs</a>.
-            </p>
+              <p>
+                If you want to learn how to query the database yourself, check
+                out <a href="http://tinyurl.com/z9jjsjb">
+                their very helpful Socrata powered API docs</a>.
+              </p>
+            </div>
           </div>
         </div>
       </div>
