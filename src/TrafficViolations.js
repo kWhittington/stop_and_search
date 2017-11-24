174198b2b64b3e4e8e7b15ca796f8d8138cee0fa
Kyle Whittington <kyle.thomas.whittington@gmail.com>
2017-11-23 22:43:52 -0600

    Add DateSelector

--- src/TrafficViolations.js
+++ src/TrafficViolations.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react'
 import { Container, Grid, Header } from 'semantic-ui-react'
-import SearchableDropdown from './SearchableDropdown'
+import Date from './Date'
+import DateSelector from './DateSelector'
 
 export default class TrafficViolations extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      startDate: Date.now()
+    }
+  }
+
+  // Needs to be binded to `this`
+  // (see https://reactjs.org/docs/handling-events.html).
+  onStartDateChange = (newDate) => {
+    console.log(`Old Date: ${this.startDate}`)
+    this.setState({ startDate: newDate })
+    console.log(`New Date: ${newDate}`)
+  }
+
+  get startDate() {
+    return this.state.startDate
+  }
+
   render() {
-    // @todo
-    //  Needs to be [{ key: 'AL', value: 'AL', text: 'Alabama' }, ...] format
-    //  but with days of the current month.
-    this.datalist = [
-      { key: 'a', value: 'a', text: 'a' }
-    ]
     return(
       <Container className='TrafficViolations'>
         <Header as='h2' icon='car' content='Traffic Violations'/>
         <Grid container>
-          <SearchableDropdown datalist={this.datalist} placeholder='Day'/>
+          <DateSelector onChange={this.onStartDateChange}
+            value={this.startDate}
+            />
         </Grid>
       </Container>)
   }
