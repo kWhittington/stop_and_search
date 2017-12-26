2cfc7d7aec490439179f9ed310b45d2657e08ef0
Kyle Whittington <kyle.thomas.whittington@gmail.com>
2017-12-25 21:36:02 -0600

    Make ReactJS copy of `master`.
    Add `DateRangeForm`, `TrafficViolationVehicleGruopsInRangeRequest`,
    `VehicleGroup`, `VehicleGroupStatistic`, and `VehicleGroupStatistics`.
    
    closes #23

--- src/TrafficViolations.js
+++ src/TrafficViolations.js
@@ -1,18 +1,19 @@
-import '@blueprintjs/core/dist/blueprint.css'
-import '@blueprintjs/datetime/dist/blueprint-datetime.css'
-import 'react-addons-css-transition-group'
-import { DateRangeInput } from '@blueprintjs/datetime'
 import React, { Component } from 'react'
-import { Container, Form, Grid, Header, Label } from 'semantic-ui-react'
+import { Container, Grid, Header } from 'semantic-ui-react'
 import Date from './Date'
+import DateRangeForm from './DateRangeForm'
 import TVCountInRangeRequest from './TrafficViolationCountInRangeRequest'
+import TVVehicleGroupsInRangeRequest from
+  './TrafficViolationVehicleGroupsInRangeRequest'
+import VehicleGroupStatistics from './VehicleGroupStatistics'
 
 export default class TrafficViolations extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      count: '...', endDate: Date.now(), startDate: Date.startOfMonth() }
-    this.updateCount(this.startDate, this.endDate)
+      count: '...', endDate: Date.now(), startDate: Date.startOfMonth(),
+      vehicleGroups: [] }
+    this.updateInfo(this.startDate, this.endDate)
   }
 
   get count() {
@@ -27,54 +28,41 @@ export default class TrafficViolations extends Component {
   // Needs to be binded to `this`
   // (see https://reactjs.org/docs/handling-events.html).
   onDateRangeChange = (selectedRange) => {
-    const [newStartDate, newEndDate] = selectedRange.map((date) => (
-      date && new Date({ year: date.getFullYear(), month: date.getMonth() + 1,
-        day: date.getDate() })
-    ))
-    this.updateCount(newStartDate, newEndDate)
-
-    this.setState({
-      endDate: newEndDate,
-      selectedEndDate: selectedRange[1],
-      selectedStartDate: selectedRange[0],
-      startDate: newStartDate
-    })
+    const [newStartDate, newEndDate] = selectedRange
+    this.updateInfo(newStartDate, newEndDate)
+    this.setState({ endDate: newEndDate, startDate: newStartDate })
   }
 
   get startDate() {
     return this.state.startDate
   }
 
-  updateCount(startDate, endDate) {
+  updateInfo(startDate, endDate) {
     new TVCountInRangeRequest({
       startDate: startDate, endDate: endDate,
       onSuccess: (count) => {
         this.setState((prevState, props) => ({ count: count }))
       }}).submit()
+    new TVVehicleGroupsInRangeRequest({
+      startDate: startDate, endDate: endDate,
+      onSuccess: (vehicleGroups) => {
+        this.setState((prevState, props) => ({ vehicleGroups: vehicleGroups }))
+      }}).submit()
+  }
+
+  get vehicleGroups() {
+    return this.state.vehicleGroups
   }
 
   render() {
     return(
       <Container className='TrafficViolations'>
         <Header as='h2' content='Traffic Violations' icon='car'/>
-        <Grid container>
+        <Grid container stackable>
           <Grid.Row>
             <Grid.Column width={4}>
-              <Form>
-                <Form.Field
-                  control={DateRangeInput}
-                  label='In Date Range'
-                  allowSingleDayRange='true' className='field'
-                  contiguousCalendarMonths='false'
-                  endInputProps={{
-                    rightElement: <Label attached='top right' tag>End</Label> }}
-                  onChange={this.onDateRangeChange}
-                  startInputProps={{
-                    rightElement: <Label attached='top right' tag>Start</Label>
-                  }}
-                  value={[this.startDate.toJSDate(), this.endDate.toJSDate()]}
-                  />
-              </Form>
+              <DateRangeForm startDate={this.startDate} endDate={this.endDate}
+                onChange={this.onDateRangeChange} />
             </Grid.Column>
             <Grid.Column textAlign='center' verticalAlign='middle' width={6}>
               <div className='ui huge horizontal statistic'>
@@ -83,6 +71,7 @@ export default class TrafficViolations extends Component {
               </div>
             </Grid.Column>
           </Grid.Row>
+          <VehicleGroupStatistics vehicleGroups={this.vehicleGroups}/>
         </Grid>
       </Container>)
   }
